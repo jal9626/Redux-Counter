200e64adc040ad1e7adaa0c6f6ad525c143faa83
jal9626 <jal9626@gmail.com>
2019-04-10 21:52:40 -0400

    redux first attempt

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,8 +13,16 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
     // Fill in the body of this case
+      return {
+        ...state + 1,
+        count: action.payload
+    }
     case DECREMENT:
     // Fill in the body of this case
+      return {
+        ...state - 1,
+        count: action.payload
+      }
     default:
       return state;
   }
